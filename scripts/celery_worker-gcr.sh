b7d51e94e145a041e884c06911b0be7135a0835e
Khushi Nahata <khushi_b220357ar@nitc.ac.in>
2025-03-05 19:28:41 +0530

    kill

--- scripts/celery_worker-gcr.sh
+++ scripts/celery_worker-gcr.sh
@@ -22,6 +22,6 @@ export NEW_RELIC_CONFIG_FILE=/etc/newrelic.ini
 if [[ -f "$NEW_RELIC_CONFIG_FILE" ]]; then
     newrelic-admin run-program celery --app=config.celery_app worker --max-tasks-per-child=6 --loglevel=info
 else
-    celery --app=config.celery_app worker --max-tasks-per-child=6 --loglevel=info && python -m http.server --directory /dummy 8000
+    celery --app=config.celery_app worker --max-tasks-per-child=6 --loglevel=info & SERVER_PID=$!; python -m http.server --directory /dummy 8000; kill $SERVER_PID
 
 fi

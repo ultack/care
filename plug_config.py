0b4ceb57149bb228e25c0f5d7a35258290d09f1d
Khushi Nahata <open.fosscell@gmail.com>
2025-03-16 20:42:55 +0530

    Update plug_config.py

--- plug_config.py
+++ plug_config.py
@@ -3,20 +3,20 @@ from plugs.plug import Plug
 
 scribe_plug = Plug(
     name="care_scribe",
-    package_name="git+https://github.com/ohcnetwork/care_scribe.git",
+    package_name="git+https://github.com/10bedicu/care_scribe.git",
     version="@master",
     configs={},
 )
 
 hcx_plugin = Plug(
     name="hcx",
-    package_name="git+https://github.com/ohcnetwork/care_hcx.git",
+    package_name="git+https://github.com/10bedicu/care_hcx.git",
     version="@hcx_emr",
     configs={},
 )
 abdm_plug = Plug(
     name="abdm",
-    package_name="git+https://github.com/ohcnetwork/care_abdm.git",
+    package_name="git+https://github.com/10bedicu/care_abdm.git",
     version="@abdm_emr",
     configs={},
 )
